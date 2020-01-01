de853f27b36a0cca65f4668947719fab152071f8
mahendra <kumarmahendra945@gmail.com>
2020-01-01 23:59:44 +0530

    yu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.discussion);
+        setContentView(R.layout.quiz_layout);
     }
 }
